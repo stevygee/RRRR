013bebbdc422f81dfb4438c750877f00e3c7f317
Stefan Grassberger <stefan.grassberger@gmail.com>
2014-08-23 17:41:42 +0200

    Player prefab and some scripts

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class MoveScript : MonoBehaviour
+{
+	// 1 - Designer variables
+	
+	/// <summary>
+	/// Object speed
+	/// </summary>
+	public Vector3 speed = new Vector3(10, 0, 0);
+	
+	/// <summary>
+	/// Moving direction
+	/// </summary>
+	public Vector3 direction = new Vector3(-1, 0, 0);
+	
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 2 - Movement
+		movement = new Vector3(
+			speed.x * direction.x,
+			speed.y * direction.y,
+			speed.z * direction.z);
+	}
+	
+	void FixedUpdate()
+	{
+		// Apply movement to the rigidbody
+		rigidbody.velocity = movement;
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector3 speed = new Vector3(50, 0, 50);
+	
+	// 2 - Store the movement
+	private Vector3 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Movement per direction
+		movement = new Vector3(
+			speed.x * inputX,
+			0,
+			0);
+		
+	}
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		rigidbody.velocity = movement;
+	}
+}
