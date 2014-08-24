9458c3c4aa36803b7dd44e01602a868e43bcdd10
Stefan Grassberger <stefan.grassberger@gmail.com>
2014-08-24 18:45:33 +0200

    Made some music

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+	public AudioClip musicLoop;
+	private bool introMusicIsPlaying = true;
+	private bool looping = false;
+
+	void Start() {
+		PlayBackgroundMusic();
+	}
+	
+	void Update() {
+		if(!audio.isPlaying) {
+			introMusicIsPlaying = false;
+		}
+		if(!introMusicIsPlaying && !looping) {
+			looping = true;
+			LoopBackgroundMusic();
+		}
+	}
+	
+	void PlayBackgroundMusic() {
+		if(audio.isPlaying == true) {
+			audio.Stop();
+		}
+		audio.Play();
+	}
+	
+	void LoopBackgroundMusic() {
+		if(audio.isPlaying == true) {
+			audio.Stop();
+		}
+		audio.clip = musicLoop;
+		audio.loop = enabled;
+		audio.Play();
+	}
+}
