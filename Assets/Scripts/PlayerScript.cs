ea44cb137fc690a24f4746a73d91d0eee0483fee
Stefan Grassberger <stefan.grassberger@gmail.com>
2014-08-25 00:20:37 +0200

    Bridge and world change state machine

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -18,21 +18,40 @@ public class PlayerScript : MonoBehaviour
 	{
 		// 3 - Retrieve axis information
 		float inputX = Input.GetAxis("Horizontal");
-		float inputY = Input.GetAxis("Vertical");
+		//float inputY = Input.GetAxis("Vertical");
 		
 		// 4 - Movement per direction
 		movement = speed * inputX;
 
-		if(inputY > 0)
-			MusicManager.Instance.SwitchWorld();
+		// 5 - World switch
+		bool worldSwitch = Input.GetButton("Fire1");
+		worldSwitch |= Input.GetButton("Fire2");
+		// Careful: For Mac users, ctrl + arrow is a bad idea
+		
+		if (worldSwitch)
+		{
+			//MusicManager.Instance.SwitchWorld();
+			WorldScript.Instance.switchWorld();
+		}
 	}
 	
 	void FixedUpdate()
 	{
 		// 5 - Move the game object
-		Vector3 movement3D = new Vector3(movement,
-		                             rigidbody.velocity.y,
-		                             rigidbody.velocity.z);
+		Vector3 movement3D;
+
+		if( rigidbody.position.x <= 3.5f && rigidbody.position.x >= -3.5f )
+		{
+			movement3D = new Vector3(movement, rigidbody.velocity.y, rigidbody.velocity.z);
+
+		}
+		else
+		{
+			movement3D = new Vector3(0, rigidbody.velocity.y, rigidbody.velocity.z);
+			float clampedX = Mathf.Clamp(rigidbody.position.x, -3.5f, 3.5f);
+			Vector3 position = new Vector3(clampedX, rigidbody.position.y, rigidbody.position.z);
+			rigidbody.position = position;
+		}
 
 		rigidbody.velocity = movement3D;
 	}
